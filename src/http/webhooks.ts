36ef47de7d8d29e4ce2d6d74867ff515ba54bf61
brunoclucio <brunoc.lucio@hotmail.com>
2025-04-24 18:15:10 -0300

    Atualizações na api de mensagens.

--- src/http/webhooks.ts
+++ src/http/webhooks.ts
@@ -1,95 +1,97 @@
-// import type { FastifyInstance, FastifyRequest } from 'fastify'
-// import type {
-//   MessageQueueProps,
-//   MessageReceivedWebhookProps,
-//   MessageSendWebhookProps,
-// } from '../@types/types'
-// import { env } from '../env'
-// import { ClientError } from '../errors/client-error'
-// import { api } from '../lib/axios'
+import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import type { MessageReceivedWebhookProps, MessageSendWebhookProps } from '../@types/types'
+import { env } from '../env'
+import { ClientError } from '../errors/client-error'
+import { api } from '../lib/axios'
 
-// export async function webhooks(app: FastifyInstance) {
-//   app.post(
-//     '/on-message-send-whatsapp-webhook',
-//     async (request: FastifyRequest<{ Body: MessageSendWebhookProps }>) => {
-//       try {
-//         console.log('body', request.body)
+export async function webhooks(app: FastifyInstance) {
+  app.post(
+    '/on-message-send-whatsapp-webhook',
+    async (
+      request: FastifyRequest<{ Body: MessageSendWebhookProps }>,
+      reply: FastifyReply<{ Body: MessageSendWebhookProps }>
+    ) => {
+      try {
+        console.log('body', request.body)
 
-//         const { zaapId, messageId, type } = request.body
+        const { zaapId, messageId, type } = request.body
 
-//         //Recupera as mensagens na fila da API.
-//         const { data: messages } = await api.get<MessageQueueProps[]>('/queue', {
-//           headers: {
-//             'Client-Token': env.CLIENT_TOKEN,
-//           },
-//           params: {
-//             page: 1,
-//             pageSize: 100,
-//           },
-//         })
+        //Recupera as mensagens na fila da API.
+        const { data: messages } = await api.get('/queue', {
+          headers: {
+            'Client-Token': env.CLIENT_TOKEN,
+          },
+          params: {
+            page: 1,
+            pageSize: 100,
+          },
+        })
 
-//         console.log('messages', messages)
+        console.log('messages', messages)
 
-//         //Caso existam mensagens em fila, verifica se a mensagem que está sendo enviada está presente.
-//         if (messages) {
-//           await Promise.all(
-//             messages.map(async message => {
-//               //Caso encontre a mensagem, insere na tabela de chats.
-//               if (message.ZaapId === zaapId) {
-//                 console.log('Encontrou a mensagem')
-//                 const chat = {
-//                   zaapId,
-//                   messageId,
-//                   type,
-//                   datIncl: new Date(),
-//                   telefone: message.Phone,
-//                   mensagem: message.Message,
-//                 }
+        //Caso existam mensagens em fila, verifica se a mensagem que está sendo enviada está presente.
+        // if (messages) {
+        //   await Promise.all(
+        //     messages.map(async message => {
+        //       //Caso encontre a mensagem, insere na tabela de chats.
+        //       if (message.ZaapId === zaapId) {
+        //         console.log('Encontrou a mensagem')
+        //         const chat = {
+        //           zaapId,
+        //           messageId,
+        //           type,
+        //           datIncl: new Date(),
+        //           telefone: message.Phone,
+        //           mensagem: message.Message,
+        //         }
 
-//                 console.log('chat', chat)
-//               }
-//             })
-//           )
-//         }
+        //         console.log('chat', chat)
+        //       }
+        //     })
+        //   )
+        // }
 
-//         return request.body
-//       } catch (error) {
-//         console.log('Error:', error)
-//         throw new ClientError('Erro no webhook de recebimento de mensagens!')
-//       }
-//     }
-//   )
+        reply.code(200).send({ message: 'Recebimento realizado com sucesso!' })
+      } catch (error) {
+        console.log('Error:', error)
+        throw new ClientError('Erro no webhook de recebimento de mensagens!')
+      }
+    }
+  )
 
-//   app.post(
-//     '/on-message-received-whatsapp-webhook',
-//     (request: FastifyRequest<{ Body: MessageReceivedWebhookProps }>) => {
-//       try {
-//         console.log('body', request.body)
-//         const { phone, messageId, type, text } = request.body
+  app.post(
+    '/on-message-received-whatsapp-webhook',
+    async (
+      request: FastifyRequest<{ Body: MessageReceivedWebhookProps }>,
+      reply: FastifyReply<{ Body: MessageReceivedWebhookProps }>
+    ) => {
+      try {
+        console.log('body', request.body)
+        const { phone, messageId, type, text } = request.body
 
-//         //Se houver mensagem.
-//         if (text) {
-//           console.log('Retorno de texto')
+        //Se houver mensagem.
+        if (text) {
+          console.log('Retorno de texto')
 
-//           const { message } = text
+          const { message } = text
 
-//           const chat = {
-//             zaapId: '',
-//             messageId,
-//             type,
-//             datIncl: new Date(),
-//             telefone: phone,
-//             mensagem: message,
-//           }
+          const chat = {
+            zaapId: '',
+            messageId,
+            type,
+            datIncl: new Date(),
+            telefone: phone,
+            mensagem: message,
+          }
 
-//           console.log('chat', chat)
-//         }
+          console.log('chat', chat)
+        }
 
-//         return request.body
-//       } catch (error) {
-//         console.log('Error:', error)
-//         throw new ClientError('Erro no webhook de envio de mensagens!')
-//       }
-//     }
-//   )
-// }
+        reply.code(200).send({ message: 'Envio realizado com sucesso!' })
+      } catch (error) {
+        console.log('Error:', error)
+        throw new ClientError('Erro no webhook de envio de mensagens!')
+      }
+    }
+  )
+}
